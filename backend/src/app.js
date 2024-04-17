49b4e0994a0a3feac68e1afba3cf622d4d4ed9cc
Kevin Dang <kevindang2008@gmail.com>
2024-04-17 16:52:57 -0500

    created POST request connecting ejs template with textyl API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,28 @@
 const textyl = require("./api/textyl");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const get = require("http");
 const path = require("path");
 const app = express();
 const port = 3000;
 
+// necessary to pull data from POST
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.use(express.static("./frontend/src"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render(
-    path.join(__dirname, "..", "..", "frontend", "src", "index"),
-  );
+  res.render(path.join(__dirname, "..", "..", "frontend", "src", "index"));
+});
+
+app.post("/", urlencodedParser, (req, res) => {
+  textyl.getLyrics(req.body.songName).then((result) => {
+    res.render(path.join(__dirname, "..", "..", "frontend", "src", "index"), {
+      lyrics: result,
+    });
+  });
 });
 
 app.listen(port, () => {
@@ -20,7 +30,3 @@ app.listen(port, () => {
 });
 
 // was just testing the get lyrics here
-textyl.getLyrics("paint the town red").then((result) => {
-  console.log(result);
-});
-
