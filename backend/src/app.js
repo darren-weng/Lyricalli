4c6fd39aa88bf21cd91f6669c28f5b67aca43c9a
Darren Weng <darren.wengs@gmail.com>
2024-04-16 21:05:15 -0500

    more setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static("./frontend/src"))
+app.use(express.static("./frontend/src"));
 
 app.get("/", (req, res) => {
-  res.sendFile(
+  res.render(
     path.join(__dirname, "..", "..", "frontend", "src", "index.html"),
   );
 });
