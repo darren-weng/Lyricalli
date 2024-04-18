a0c2e9d26c35f928c4e09b4405448695fd40883b
Darren Weng <darren.wengs@gmail.com>
2024-04-18 18:22:30 -0500

    added functionality to lastfm.js
    - added getTopArtists to fetch information
    - added filterTopArtists to get a list of the top 100 artists

--- backend/src/api/lastfm.js
+++ backend/src/api/lastfm.js
@@ -1,4 +1,36 @@
 require("dotenv").config();
 const apiKey = process.env.LASTFM_API_KEY;
 
-console.log(apiKey);
+// fetches the top 100 artists in a country
+async function getTopArtists(country) {
+  country = encodeURIComponent(country);
+  const url =
+    "https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=" +
+    country +
+    "&limit=100" +
+    "&api_key=" +
+    apiKey +
+    "&format=json";
+
+  const response = await fetch(url, {
+    method: "GET",
+  });
+
+  const topArtists = await response.text();
+  return filterTopArtists(topArtists);
+}
+
+// gives a list of the top 100 artists
+function filterTopArtists(text) {
+  let artists = [];
+  text = JSON.parse(text);
+
+  for (let i = 0; i < text.topartists.artist.length; ++i) {
+    artists.push(text.topartists.artist[i].name);
+  }
+
+  return artists;
+}
+
+// testing here
+getTopArtists("united states").then((res) => console.log(res));
