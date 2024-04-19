5b45497a408df0d56fd888fe9ddb837c4420b4aa
Darren Weng <darren.wengs@gmail.com>
2024-04-18 19:15:31 -0500

    getTopArtists update
    - added support for global top artists and country

--- backend/src/api/lastfm.js
+++ backend/src/api/lastfm.js
@@ -3,14 +3,22 @@ const apiKey = process.env.LASTFM_API_KEY;
 
 // fetches the top 100 artists in a country
 async function getTopArtists(country) {
-  country = encodeURIComponent(country);
-  const url =
-    "https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=" +
-    country +
-    "&limit=100" +
-    "&api_key=" +
-    apiKey +
-    "&format=json";
+  let url = "";
+
+  if (country) {
+    url =
+      "https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=" +
+      country +
+      "&limit=100" +
+      "&api_key=" +
+      apiKey +
+      "&format=json";
+  } else {
+    url =
+      "https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" +
+      apiKey +
+      "&format=json";
+  }
 
   const response = await fetch(url, {
     method: "GET",
@@ -25,12 +33,18 @@ function filterTopArtists(text) {
   let artists = [];
   text = JSON.parse(text);
 
-  for (let i = 0; i < text.topartists.artist.length; ++i) {
-    artists.push(text.topartists.artist[i].name);
+  if (text.artists != undefined) {
+    for (let i = 0; i < text.artists.artist.length; ++i) {
+      artists.push(text.artists.artist[i].name);
+    }
+  } else {
+    for (let i = 0; i < text.topartists.artist.length; ++i) {
+      artists.push(text.topartists.artist[i].name);
+    }
   }
 
   return artists;
 }
 
 // testing here
-getTopArtists("united states").then((res) => console.log(res));
+getTopArtists().then((res) => console.log(res));
