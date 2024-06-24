659d854bab88db6d7b9177c621bf94359b9c9cca
Kevin Dang <kevindang2008@gmail.com>
2024-06-24 15:32:07 -0500

    changed api id name

--- frontend/src/script/play.js
+++ frontend/src/script/play.js
@@ -1,6 +1,6 @@
 // adds Youtube iframe api
 const tag = document.createElement("script");
-tag.id = "iframe-demo";
+tag.id = "iframe-script";
 tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
